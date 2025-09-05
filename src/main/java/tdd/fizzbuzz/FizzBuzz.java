62f77784c075615cc175c4a4648636f5c1ad8db3
lyle.li <lily7@oocl.com>
2025-09-05 11:44:33 +0800

    feat:complete FizzBuzz.java and FizzBuzzTest.java

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,19 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
+    public String countOff(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return FIZZ_BUZZ;
+        } else if (number % 3 == 0) {
+            return FIZZ;
+        } else if (number % 5 == 0) {
+            return BUZZ;
+        } else {
+            return String.valueOf(number);
+        }
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,49 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_normal_number_string_when_input_normal_number() {
+        String expectResult = "1";
+        //Given
+        int inputNumber = 1;
+        //When
+        String result = new FizzBuzz().countOff(inputNumber);
+        //Then
+        assertEquals(expectResult,result);
+    }
+    @Test
+    public void should_return_Fizz_when_input_number_that_is_multiple_of_3() {
+        String expectResult = FizzBuzz.FIZZ;
+        //Given
+        int inputNumber = 3;
+        //When
+        String result = new FizzBuzz().countOff(inputNumber);
+        //Then
+        assertEquals(expectResult,result);
+    }
+    @Test
+    public void should_return_Buzz_when_input_number_that_is_multiple_of_5() {
+        String expectResult = FizzBuzz.BUZZ;
+        //Given
+        int inputNumber = 5;
+        //When
+        String result = new FizzBuzz().countOff(inputNumber);
+        //Then
+        assertEquals(expectResult,result);
+    }
+    @Test
+    public void should_return_FizzBuzz_when_input_number_that_is_multiple_of_15() {
+        String expectResult = FizzBuzz.FIZZ_BUZZ;
+        //Given
+        int inputNumber = 15;
+        //When
+        String result = new FizzBuzz().countOff(inputNumber);
+        //Then
+        assertEquals(expectResult,result);
+    }
+
 }
